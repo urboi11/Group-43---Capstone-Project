8947ed70215c7c20f0bed556a7d8da464bdb3684
Jason Welsh <welshjason03@gmail.com>
2025-10-12 18:16:45 -0400

    Adding additional files

--- app/piiscannerapp.py
+++ app/piiscannerapp.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PySide6.QtCore import QSize
 from PiiScanner import Ui_Form
 import sys, os
 
@@ -9,7 +10,16 @@ class MainWindow(QMainWindow, Ui_Form):
 
         self.setupUi(self)
 
-        self.browseButton.clicked.connect(self.open_file_browser)
+        self.setFixedSize(QSize(650,500))
+
+        self.FileBrowseButton.clicked.connect(self.open_file_browser)
+
+        self.ScanFilesButton.clicked.connect(self.SwitchToFilePanel(2))
+
+        self.FileMainMenuButton.clicked.connect(self.SwitchToFilePanel(0))
 
     def open_file_browser(self):
         fileName = QFileDialog.getOpenFileName(self, "Find Files..", os.getcwd(), "Text Files(*.txt)")
+    
+    def SwitchToFilePanel(self, index):
+        self.stackedWidget.setCurrentIndex(index)
--- app/scan.py
+++ app/scan.py
@@ -2,7 +2,7 @@
 import sys
 import argparse, json, os, fnmatch, glob, pathlib, time, yaml
 from PySide6.QtWidgets import QApplication
-from piiscannerapp import MainWindow
+from PiiScannerApp import MainWindow
 from rich.progress import track
 from extract import read_txt, read_docx, read_pdf
 from infer import PiiModel
