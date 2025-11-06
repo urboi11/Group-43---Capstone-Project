27f0a92f5c92a7ecd1b339ca5326617d24eaa1e5
CalebMuck <124749431+CalebMuck@users.noreply.github.com>
2025-11-06 11:27:08 -0500

    Delete scripts directory

--- scripts/modelsave.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModelForTokenClassification, AutoTokenizer
-
-ckpt = "pii-lab/experiments/baseline/model/checkpoint-1500"  # replace with your last checkpoint folder
-model = AutoModelForTokenClassification.from_pretrained(ckpt)
-tokenizer = AutoTokenizer.from_pretrained(ckpt)
-
-model.save_pretrained("pii-lab/experiments/baseline/final-model")
-tokenizer.save_pretrained("pii-lab/experiments/baseline/final-model")
